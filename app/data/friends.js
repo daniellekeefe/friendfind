9618590197a1970e176ab45e1b9f0f305f3e161a
olive <daniellek@mobilefuse.com>
2019-12-20 14:49:52 -0500

    some updates

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Danielle Keefe',
+      profilePic: '',
+      scores: [
+        "2",
+        "5",
+        "4",
+        "2",
+        "1",
+        "3",
+        "5",
+        "5",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
--- app/server.js
+++ app/server.js
@@ -1,3 +1,9 @@
 // Dependencies for various node packages
 let express = require("express");
 let path = require("path");
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('listening on port %s', port));
+
