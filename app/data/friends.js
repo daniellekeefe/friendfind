5f8e2d3ab98d74269bd58eac4873b40eaa0707a5
olive <daniellek@mobilefuse.com>
2019-12-22 14:16:55 -0500

    got images to display

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
 // array of objects (1-5 that will be summed and compared to the user input and help define BFF)
 let friends = [{
-        name: 'Wayne Gretzky',
-        photo: 'https://images.app.goo.gl/3xs5jBrMBoHy59477',
-        scores: ["2", "5", "4", "2", "1", "3", "5", "5", "3", "4"]
+        "name": 'Wayne Gretzky',
+        "photo": 'https://cdn-s3.si.com/s3fs-public/wayne-gretzky-1981.jpg',
+        "scores": ["2", "5", "4", "2", "1", "3", "5", "5", "3", "4"]
     },
     {
-        name: 'Tom Brady',
-        photo: 'https://images.app.goo.gl/oKT6pNAYz15fMcef7',
-        scores: ["5", "5", "4", "2", "3", "3", "1", "2", "3", "1"]
+        "name": 'Tom Brady',
+        "photo": 'https://sportshub.cbsistatic.com/i/r/2019/10/24/f583c5fc-de8b-43a6-9050-2ee5a1c36195/thumbnail/1200x675/2e201dc887d92b91644e63e8869275df/bradyne-2.jpg',
+        "scores": ["5", "5", "4", "2", "3", "3", "1", "2", "3", "1"]
     },
     {
-        name: 'Cristiano Ronaldo',
-        photo: 'https://images.app.goo.gl/c6Eeq8761oMBnZ5b8',
-        scores: ["4", "2", "1", "2", "5", "1", "5", "2", "3", "1"]
+        "name": 'Cristiano Ronaldo',
+        "photo": 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F09%2Fcristiano-ronaldo-nike-162-million-euros-contract-details-1.jpg?q=75&w=800&cbr=1&fit=max',
+        "scores": ["4", "2", "1", "2", "5", "1", "5", "2", "3", "1"]
     },
     {
-        name: 'Serena Williams',
-        photo: 'https://images.app.goo.gl/HW3bH5HAZyjfjXd57',
-        scores: ["5", "1", "2", "2", "1", "2", "3", "3", "4", "1"]
+        "name": 'Serena Williams',
+        "photo": 'https://cdn.britannica.com/70/149370-050-F2F4BEE5/Serena-Williams-US-Open-2004.jpg',
+        "scores": ["5", "1", "2", "2", "1", "2", "3", "3", "4", "1"]
     },
     {
-        name: 'Lindsay Vonn',
-        photo: 'https://images.app.goo.gl/GGd7tPLGWZTKt4Xg9',
-        scores: ["3", "3", "3", "1", "4", "4", "3", "4", "2", "1"]
+        "name": 'Lindsay Vonn',
+        "photo": 'https://cdn.britannica.com/29/199929-050-6232CA7A/Lindsey-Vonn-2015.jpg',
+        "scores": ["3", "3", "3", "1", "4", "4", "3", "4", "2", "1"]
     },
     {
-        name: 'Ronda Rousey',
-        photo: 'https://images.app.goo.gl/sRUSL3iGY2JdCX5c9',
-        scores: ["4", "4", "1", "2", "3", "1", "3", "3", "2", "1"]
+        "name": 'Ronda Rousey',
+        "photo": 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492185209/articles/2015/03/11/the-rise-of-rowdy-ronda-rousey-the-14-second-assassin/150310-stern-rousey-tease_bixe8j',
+        "scores": ["4", "4", "1", "2", "3", "1", "3", "3", "2", "1"]
     }
 ];
 //line below exports the module
