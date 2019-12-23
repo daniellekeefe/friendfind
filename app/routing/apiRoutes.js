f51f3410722b3ad08b3714aec711c336064eb5fa
olive <daniellek@mobilefuse.com>
2019-12-22 19:14:44 -0500

    updated some messaging and notes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 const friends = require("../data/friends");
 
 module.exports = function (app) {
-    // Return all friends found in friends.js as JSON
+    // Return friends found in friends.js as JSON
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
